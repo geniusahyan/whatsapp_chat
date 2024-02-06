a8f3e4568b7d28dabd1c364d95708a97bf5e7a84
geniusahyan <aslamalam078523@gmail.com>
2024-02-07 00:21:11 +0530

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Login from './components/Login'
+import { GoogleOAuthProvider } from '@react-oauth/google' 
 
 function App() {
+  const clientId = import.meta.env.GOOGLE_CLIENT_ID || "530508373190-1tstvndeg0hid0clr68r0t3mumjk2ef9.apps.googleusercontent.com" ;
   return (
-    <Login />
+    <>
+     <GoogleOAuthProvider clientId={clientId} >
+        <Login />
+     </GoogleOAuthProvider>
+    </>
   )
 }
 
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,14 +1,117 @@
 import React from 'react'
+import { AppBar, styled, Box, Dialog, Typography, List, ListItem, ListItemText} from '@mui/material'
+import Portfolio from '../../public/portfolio.png'
+import { GoogleLogin } from '@react-oauth/google'
+import { jwtDecode } from "jwt-decode";
+export default function Login() {
 
-function Login() {
+    const Header = styled(AppBar)`
+        height:14rem;
+        width:100%;
+        box-shadow:none;
+        background-color:#00a884;
+    `
+    const Wrapper = styled(Box)`
+        margin:0;
+        padding:0;
+    `
+    const Left = styled(Box)`
+        height:100%;
+        box-sizing: border-box;
+        width:60%;
+        padding: 3rem;
+        user-select: none;
+        `
+        const Right = styled(Box)`
+        height:100%;
+        width:40%;
+        box-sizing: border-box;
+        padding: 1rem;
+    `
+    const Dialogbox = {
+        position: 'absolute',
+        top: '48%',
+        left: '47%',
+        transform: 'translate(-50%, -50%)',
+        width: '80vw',
+        maxWidth: '100%',
+        height:'80%',
+        bgcolor: 'background.paper',
+        boxShadow: 'none',
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap:'flex-wrap',
+        color: '#525252'
+    }
+    const ListLi = styled(List)`
+        margin-top:1rem;
+    `
+    const Qrcode = styled('img')({
+        width: '20rem',
+        height: '60%'
+    })
+    const GoogleLog = styled(Box)`
+        width: 12rem;
+        heigth:1rem;
+        margin: 1rem auto;
+    `
+    const Ahyan = styled(Typography)`
+        text-align:center;
+        font-size:2rem;
+        font-weight:bold;
+        color:#fff;
+        margin-top:1rem;
+    `
+
+    const LoginSuccess = (data)=>{
+        console.log(jwtDecode(data.credential))
+    }
+    const LoginError = ()=>{
+        console.log('login failed')
+    }
+
+
   return (
     <>
-        <div className="wrapper w-screen h-screen ">
-            <div className="header w-full h-56 bg-[#00a884] "></div>
-            
-        </div>
+    <Wrapper>
+        <Header>
+            <Ahyan>
+                Ahyan
+            </Ahyan>
+        </Header>
+        <Dialog open={true} PaperProps={{sx:Dialogbox}} >
+            <Left>
+                <Typography variant='h5' > To use whatsapp on your device </Typography>
+                <ListLi>
+                    <ListItem>
+                        <ListItemText primary="1. Install whatsapp on your device" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="2. Open whatsapp on your device" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="3. Go to settings" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="4. Kuch mat karo Direct Google se Login karlo" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="5. Don't do anything just Logged in using Google " />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemText primary="6. Scanning function nahi hai but QR-Scanner se scan sakte ho 💝 " />
+                    </ListItem>
+                </ListLi>
+
+                <GoogleLog>
+                    <GoogleLogin onSuccess={LoginSuccess} onError={LoginError} />
+                </GoogleLog>
+            </Left>
+            <Right>
+                <Qrcode src={Portfolio} width={'12rem'} draggable='false' alt="qrcode" />
+            </Right>
+        </Dialog>
+    </Wrapper>
     </>
   )
 }
-
-export default Login
