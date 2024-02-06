90f73082fa9e04eea7540d879abc6b23346b2827
geniusahyan <aslamalam078523@gmail.com>
2024-02-06 21:05:37 +0530

    first commit

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <>
+        <div className="wrapper w-screen h-screen ">
+            <div className="header w-full h-56 bg-[#00a884] "></div>
+            
+        </div>
+    </>
+  )
+}
+
+export default Login
