06530f8bb71c942ce9942ea5aa4fe7723f3d5bf4
geniusahyan <aslamalam078523@gmail.com>
2024-02-07 12:49:59 +0530

    rightBottom complete

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Dialog, Box, styled, List, ListItem, Divider, Typography } from '@mui/material' 
 import Portfolio from '../../public/portfolio.png'
-import { Call, Chat, Done, MoreVert, MotionPhotosAuto, Troubleshoot, Videocam } from '@mui/icons-material';
+import { Call, Chat, Done, MoreVert, MotionPhotosAuto, Troubleshoot, Videocam, DoneAll, KeyboardVoice, AddCircle, AddReaction } from '@mui/icons-material';
 import EmptyWhatsapp from '../../public/emptyWhatsapp.jpg'
 import Background from '../../public/background.jpg'
 
@@ -59,6 +59,9 @@ function Message() {
     &::-webkit-scrollbar-thumb {
         background-color: #622bb7;
     }
+    &::-webkit-scrollbar-track {
+        background: transparent;
+    }
 `
 
     const Left = styled(Box)`
@@ -75,6 +78,7 @@ function Message() {
         box-sizing: border-box;
         padding: 0;
         overflow:hidden;
+        position:relative;
         & > img{
             filter: blur(3px);
         }
@@ -190,18 +194,163 @@ function Message() {
         align-items:center;
     `
     const ChatDialog = styled(Box)`
-        
+        height:100%;
     `
     const RigthBottom = styled(Box)`
         height: 90%;
         box-sizing: border-box;
         padding:0;
+        display:flex;
+        flex-direction:column;
         box-sizing: border-box;
+        background:url(${Background});
+        position:relative;
         overflow:hidden;
         & > img{
             filter: brightness(110%);
+            position:absolute;
+            top:0;
+            left:0;
         }
     `
+    const BackImage = styled('img')({
+        width: '100%',
+        height: '100vh',
+        objectFit: 'cover',
+        cursor: 'pointer',
+        zIndex:'-1',
+    })
+
+    const ChatTextBox = styled(Box)`
+        width:100%;
+        height:100%;
+        box-sizing: border-box;
+        padding:2rem;
+        margin:0;
+        overflow-y:scroll;
+        -webkit-overflow-scrolling: touch; /* for smooth scrolling on iOS */
+        &::-webkit-scrollbar {
+            width: 3px;
+        }
+        &::-webkit-scrollbar-track {
+            background: transparent;
+        }
+        &::-webkit-scrollbar-thumb {
+            background: #ddd;
+        }
+    `
+    const TextMessage = styled(ListItem)`
+        background:#256358 !important;
+        padding:0.4rem 0.6rem;
+        color:#fff;
+        font-size:14px;
+        text-align:left;
+        max-width:70%;
+        padding-bottom:1rem;
+        margin:0;
+        margin-bottom:0.4rem;
+        word-wrap:break-word;
+        position:relative;
+        &:hover{
+            cursor:pointer;
+            background:#256358!important;
+        }
+        border-top-right-radius:7px;
+        border-bottom-right-radius:7px;
+    `
+    const TextMessages = styled(ListItem)`
+        background:#256358 !important;
+        padding:0.4rem 0.6rem;
+        color:#fff;
+        font-size:14px;
+        max-width:70%;
+        max-height:7rem;
+        margin:0;
+        margin-left:auto;
+        margin-bottom:0.4rem;
+        word-wrap:break-word;
+        &:hover{
+            cursor:pointer;
+            background:#256358!important;
+        }
+        border-top-left-radius:7px;
+        border-bottom-left-radius:7px;
+        & > p{
+            margin-top:0.4rem;
+            text-overflow: ellipsis;
+            overflow: hidden;
+            height:7rem;
+        }
+    `
+    const TimeStamp = styled(Typography)`
+        font-size:12px;
+        position:absolute;
+        right:0.6rem;
+        bottom:0.6rem;
+        opacity:0.6;
+        display:flex;
+        align-items:center;
+        & > svg{
+            margin:0;
+            padding:0;
+            font-size:1.2rem;
+        }
+    `
+    const SendBox = styled(Box)`
+        background-color:#e7e7e7;
+        width:100%;
+        height:3.5rem;
+        padding:0 1rem;
+        box-sizing:border-box;
+        display:flex;
+        justify-content:space-between;
+        align-items:center;
+        & > div{
+            background:white;
+            height:2rem;
+            width:2rem;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            color:#525252;
+            border-radius:50%;
+            & > svg {
+                margin: 0 0.8rem;
+                font-size: 1.7rem;
+                &:hover{
+                    cursor:pointer;
+                    color:#00a884;
+                    transform:scale(1.2);
+                    transition:0.3s;
+    
+                }
+            }
+            &:hover{
+                cursor:pointer;
+                transition:0.1s;
+            }
+        }
+    `
+    const InputBox = styled(Box)`
+        width:37rem !important;
+        border-radius:6px !important;
+        height:2.5rem !important;
+        box-sizing: border-box;
+    `
+    const Input = styled('input')({
+        width: '100%',
+        height: '100%',
+        border: 'none',
+        outline: 'none',
+        padding: '0 1rem',
+        boxSizing: 'border-box',
+        fontSize: '1rem',
+        color: '#525252',
+        '&:focus': {
+            border: 'none',
+            boxShadow: 'none',
+        },
+    })
 
   return (
     <>
@@ -271,7 +420,39 @@ function Message() {
                     </PersonChat>
                     </RightTop>
                     <RigthBottom>
-                        <img src={Background} alt="" />
+                        {/* <BackImage src={Background} alt="" /> */}
+                        <ChatTextBox>
+                            <List>
+                                <TextMessage>
+                                    <Typography>
+                                        hello Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi delectus facilis fugit.Hey, how are you? I miss you!
+                                        hello Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi delectus facilis fugit.Hey, how are you? I miss you!
+                                    </Typography>
+                                    <TimeStamp>
+                                        12:00am
+                                        <DoneAll />
+                                    </TimeStamp>
+                                </TextMessage>
+                                <TextMessages>
+                                    <Typography>
+                                        hello Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi delectus facilis fugit.Hey, how are you? I miss you!
+                                        hello Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi delectus facilis fugit.Hey, how are you? I miss you!
+                                    </Typography>
+                                </TextMessages>
+                            </List>
+                        </ChatTextBox>
+                        <SendBox>
+                            <Box>
+                                <AddCircle />
+                            </Box>
+                            <InputBox>
+                                <AddReaction />
+                                <Input placeholder='Type message here..' type="text" name="" id="" />
+                            </InputBox>
+                            <Box>
+                                <KeyboardVoice />
+                            </Box>
+                        </SendBox>
                     </RigthBottom>
                 </ChatDialog>
             </Right>
---
+++ frontend/src/components/SendInputCom.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Dialog, Box, styled, List, ListItem, Divider, Typography } from '@mui/material' 
+import Portfolio from '../../public/portfolio.png'
+import { Call, Chat, Done, MoreVert, MotionPhotosAuto, Troubleshoot, Videocam, DoneAll, KeyboardVoice, AddCircle, AddReaction } from '@mui/icons-material';
+
+function SendInputCom() {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default SendInputCom
